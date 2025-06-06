003b1e3d07929684cc7277ff4e761657be2600de
rijwankabir <rijwan241-15-840@diu.edu.bd>
2025-06-06 09:05:11 +0600

    Initial commit

---
+++ first.js
@@ -0,0 +1,9 @@
+let num1 = Number(prompt("Enter the first number:"));
+let num2 = Number(prompt("Enter the second number:"));
+let sum = num1 + num2;
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
+
+let firstName = prompt("Enter your first name:");
+let lastName = prompt("Enter your last name:");
+let fullName = firstName + " " + lastName;
+console.log(`Full name is: ${fullName}`);
