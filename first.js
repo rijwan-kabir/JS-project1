d7b338b6a7f93b2556357da901a93f100605d936
rijwankabir <rijwan241-15-840@diu.edu.bd>
2025-06-06 17:07:51 +0600

    Add Display

--- first.js
+++ first.js
@@ -1,9 +1,11 @@
 let num1 = Number(prompt("Enter the first number:"));
 let num2 = Number(prompt("Enter the second number:"));
 let sum = num1 + num2;
-console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
 
 let firstName = prompt("Enter your first name:");
 let lastName = prompt("Enter your last name:");
 let fullName = firstName + " " + lastName;
-console.log(`Full name is: ${fullName}`);
+
+let result = document.getElementById("display");
+result.innerHTML = `The sum of ${num1} and ${num2} is: ${sum}`;
+result.innerHTML += `<br>Full name is: ${fullName}`;
